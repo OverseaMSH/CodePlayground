b8a4e1d481405ae6c157c96de79818620a5036e9
Mohammad Sadegh Hemati <sadegh1384ordibehesht@gmail.com>
2024-06-01 02:28:53 +0330

    Make python folder and make python-in-100-days-udemy folder inside it.Code day1 project and also make the python-in-100-days-udemy documentation.

---
+++ python/python-in-100-days-udemy/day1.py
@@ -0,0 +1,4 @@
+print("Welcome to Sadegh day 1 pyhton in 100 days Band Name Generator")
+b1 = input("What's name of the city you grew up?\n")
+b2 = input("What's your pet's name?\n")
+print(f"Your band name could be {b1} {b2}")
