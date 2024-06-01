a1d9c6c6921b51e96b522b07628fb7cc1001e931
Mohammad Sadegh Hemati <sadegh1384ordibehesht@gmail.com>
2024-06-01 21:52:08 +0330

    Added additional game logic for "left" path, including lake scenario and island door choices.

--- day3.py
+++ day3.py
@@ -26,3 +26,9 @@ step1 = input(
     "you're at a cross road. Where do you want to go? Type 'left' or 'right\n'").lower()
 if step1 == "right":
     print("You fell into a hole. Game over!")
+elif step1=="left":
+    step2=input("You come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat or type 'swim' to swim across.\n").lower()
+    if step2=="swim":
+        print("You got attacked by an angry trout. Game over!")
+    elif step2=="wait":
+        step3=input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which color do you choose?\n").lower()
