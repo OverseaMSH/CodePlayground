8275934a2b5d7401e6aa40556e6b42d810644de0
Mohammad Sadegh Hemati <sadegh1384ordibehesht@gmail.com>
2024-06-02 01:27:45 +0330

    Added user input and computer choice logic to day4 Rock, Paper, Scissors game.

--- python/python-in-100-days-udemy/day4.py
+++ python/python-in-100-days-udemy/day4.py
@@ -26,3 +26,19 @@ scissors = '''
 ---.__(___)
 '''
 print("Welcome to Sadegh day 4 pyhton in 100 days Rock, Paper and Scissors.")
+playerChoiceNum = int(
+    input("What do ypu choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
+)
+if playerChoiceNum == 0:
+    print("You chose:\n", rock)
+elif playerChoiceNum == 1:
+    print("You chose:\n", paper)
+elif playerChoiceNum == 2:
+    print("You chose:\n", scissors)
+computerChoiceNum = random.randint(0, 2)
+if computerChoiceNum == 0:
+    print("Computer chose:\n", rock)
+elif computerChoiceNum == 1:
+    print("Computer chose:\n", paper)
+elif computerChoiceNum == 2:
+    print("Computer chose:\n", scissors)
