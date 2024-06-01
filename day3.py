c1c53fddb4abc1a8b56c16c0bcfda0863cd1a058
Mohammad Sadegh Hemati <sadegh1384ordibehesht@gmail.com>
2024-06-01 22:54:04 +0330

    Complete day3 project

--- day3.py
+++ day3.py
@@ -23,12 +23,21 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Sadegh day 3 pyhton in 100 days Treasure Island.")
 print("Your mission is to find the treasure.")
 step1 = input(
-    "you're at a cross road. Where do you want to go? Type 'left' or 'right\n'").lower()
+    "you're at a cross road. Where do you want to go? Type 'left' or 'right'\n").lower()
 if step1 == "right":
     print("You fell into a hole. Game over!")
-elif step1=="left":
-    step2=input("You come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat or type 'swim' to swim across.\n").lower()
-    if step2=="swim":
+elif step1 == "left":
+    step2 = input("You come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat or type 'swim' to swim across.\n").lower()
+    if step2 == "swim":
         print("You got attacked by an angry trout. Game over!")
-    elif step2=="wait":
-        step3=input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which color do you choose?\n").lower()
+    elif step2 == "wait":
+        step3 = input(
+            "You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which color do you choose?\n").lower()
+        if step3 == "yellow":
+            print("You found the treasure! You win!")
+        elif step3 == "red":
+            print("It's a room full of fire. Game Over!")
+        elif step3 == "blue":
+            print("You entered a room of beasts. Game Over!")
+# Flowchart
+# https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
