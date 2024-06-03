a03c93d75683ed79a5e41b8284011a85bd99bd2e
Mohammad Sadegh Hemati <sadegh1384ordibehesht@gmail.com>
2024-06-03 23:40:00 +0330

    Added password generation functionality to day5.py

--- python/python-in-100-days-udemy/day5.py
+++ python/python-in-100-days-udemy/day5.py
@@ -1,6 +1,19 @@
+import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
            'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to Sadegh day 5 python in 100 days PyPassword Generator!")
+lettersLength = int(
+    input("How many letters would you like in your password?\n"))
+symbolsLength = int(
+    input("How many symbols would you like in your password?\n"))
+numbersLength = int(
+    input("How many numbers would you like in your password?\n"))
+passwordArr = []
+passwordArr.extend(random.choices(letters, k=lettersLength))
+passwordArr.extend(random.choices(symbols, k=symbolsLength))
+passwordArr.extend(random.choices(numbers, k=numbersLength))
+random.shuffle(passwordArr)
+print("".join(passwordArr))
