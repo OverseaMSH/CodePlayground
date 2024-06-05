fc73b2f970278fb757a6a486fb5180dc2d66d88f
Mohammad Sadegh Hemati <sadegh1384ordibehesht@gmail.com>
2024-06-06 00:46:55 +0330

    "Added Caesar cipher encryption and decryption functions, implemented user input and output, and modified the program flow to handle encoding and decoding options."

--- python/python-in-100-days-udemy/day8.py
+++ python/python-in-100-days-udemy/day8.py
@@ -1,3 +1,4 @@
+import time
 art = (
     " ,-----.                                  \n"
     "'  .--./,--,--.,---.  ,---. ,--,--.,--.--. \n"
@@ -13,10 +14,48 @@ art = (
     " `-----'`--'|  |-' `--' `--'`----'`--'      \n"
     "            `--'                            \n"
 )
+
+alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def encrypt(text, shiftNum):
+    newText = ""
+    for char in text:
+        if char in alphabets:
+            newText += alphabets[(alphabets.index(char)+shiftNum) % 26]
+        else:
+            newText += char
+    return newText
+
+
+def decrypt(text, shiftNum):
+    newText = ""
+    for char in text:
+        newText += alphabets[(alphabets.index(char)-shiftNum) % 26]
+    return newText
+
+
+print(encrypt("hello", 4))
 while True:
     print(art+"\n")
     print("Welcome to sadegh day 8 python in 100 days cryption app")
     selection = input(
         "Enter 'encode' to encrypt, 'decode' to decrypt:\n").lower()
-    text = input(f"Enter the text to be {selection}d: ").lower()
-    shiftNum = int(input("Enter the shift number: "))
+    if selection == "encode":
+        text = input("Enter the text to be encoded: ").lower()
+        shiftNum = int(input("Enter the shift number: "))
+        print(f"Encoded text: {encrypt(text, shiftNum)}")
+        finished = input("Are you finished? (yes/no): ").lower()
+        if finished == 'yes':
+            break
+    elif selection == "decode":
+        text = input("Enter the text to be decoded: ").lower()
+        shiftNum = int(input("Enter the shift number: "))
+        print(f"Decoded text: {decrypt(text, shiftNum)}")
+        finished = input("Are you finished? (yes/no): ").lower()
+        if finished == 'yes':
+            break
+    else:
+        print("Invalid input. Please enter 'encode', 'decode'.")
+        time.sleep(1)
