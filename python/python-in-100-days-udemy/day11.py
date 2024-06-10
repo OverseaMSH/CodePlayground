05db07d45e413470f438d7a6dde304b92d3f6e17
Mohammad Sadegh Hemati <sadegh1384ordibehesht@gmail.com>
2024-06-10 16:22:47 +0330

    Refactored Blackjack game code: added import statements, modified blackjackArt string, updated choose_card function, added new functions show_first_hand_of_player and show_first_hand_of_computer, and implemented game loop with input handling.

--- python/python-in-100-days-udemy/day11.py
+++ python/python-in-100-days-udemy/day11.py
@@ -1,14 +1,16 @@
 import random
 import sys
-blackjackArt = """
+import os
+import time
+blackjackArt = r"""
   ____  _            _    _            _    
  |  _ \| |          | |  (_)          | |   
  | |_) | | __ _  ___| | ___  __ _  ___| | __
  |  _ <| |/ _` |/ __| |/ / |/ _` |/ __| |/ /
  | |_) | | (_| | (__|   <| | (_| | (__|   < 
- |____/|_|\__,_|\___|_|\_\ |\__,_|\___|_|\_\\
+ |____/|_|\__,_|\___|_|\_\ |\__,_|\___|_|\_\
                         _/ |                
-                       |__/    \n             
+                       |__/                   
   _____    _____    _____    _____
  |\ ~ /|  |\ ~ /|  |\ ~ /|  |\ ~ /|
  |}}:{{|  |}}:{{|  |}}:{{|  |}}:{{|
@@ -17,8 +19,7 @@ blackjackArt = """
  |/_~_\|  |/_~_\|  |/_~_\|  |/_~_\|
 """
 
-print(blackjackArt)
-print("Welcome to Sadegh python in 100 days blackjack")
+
 spades = [
     ("""
    _____ 
@@ -443,33 +444,71 @@ clubs = [
   |____A|
          """, (1, 11)),
 ]
-
 deck = spades + hearts + diamonds + clubs
-def choose_card(list):
-    random.shuffle(list)
-    randomCard = random.choice(list)
-    if randomCard[1]==(1, 11):
-        randomCard = (randomCard[0], 11)
-    list.remove(randomCard)
+
+def choose_card(deck):
+    random.shuffle(deck)
+    randomCard = random.choice(deck)
+    if randomCard[1] == (1, 11):
+        if randomCard[1] + 11 > 21:
+            randomCard = (randomCard[0], 1)
+        else:
+            randomCard = (randomCard[0], 11)
+    deck.remove(randomCard)
     return randomCard
-def show_first_hand_player():
+
+
+
+def show_first_hand_of_player():
     print("Your cards:")
-    pCard1,pCard2=choose_card(deck),choose_card(deck)
+    pCard1, pCard2 = choose_card(deck), choose_card(deck)
+    print(pCard1[0], pCard2[0])
+    print("Current score:", pCard1[1] + pCard2[1], "\n")
+    return [pCard1, pCard2]
 
-    print(pCard1[0],pCard2[0])
-    print("Current score:",pCard1[1]+pCard2[1])
-    
+def show_first_hand_of_computer():
+    print("Computer cards:")
+    cCard1 = choose_card(deck)
+    print(cCard1[0])
+    return [cCard1]
 
-# while True:
-#     try:
-#         start = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower()
-#         if start!='n' and start !='y':
-#             raise ValueError
-#         else:
-#             if start == 'y':
-#                 pass
-#             else:
-#                 sys.exit()
-#     except ValueError:
-#         print("Pleaze only type 'y' or 'n': ")
-show_first_hand_player()
+def player_stand(computerCards, playerCards):
+    while sum(card[1] for card in computerCards) < 22:
+        computerCards.append(choose_card(deck))
+    return computerCards
+
+while True:
+    try:
+        print(blackjackArt)
+        print("Welcome to Sadegh python in 100 days blackjack")
+        start = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower()
+        if start != 'n' and start != 'y':
+            raise ValueError
+        else:
+            if start == 'y':
+                playerCards = show_first_hand_of_player()
+                computerCards = show_first_hand_of_computer()
+                try:
+                    anotherCard = input("Type 'y' to hit or type 'n' to stand: ").lower()
+                    if anotherCard != 'n' and anotherCard != 'y':
+                        raise ValueError
+                    else:
+                        if anotherCard == 'y':
+                            pass  # You can implement the 'hit' functionality here
+                        else:
+                            computerCards = player_stand(computerCards, playerCards)
+                            print("Computer's final cards:")
+                            for card in computerCards:
+                                print(card[0])
+                            print("Computer's final score:", sum(card[1] for card in computerCards))
+                            print("Your final score:", sum(card[1] for card in playerCards))
+
+                except ValueError:
+                    print("Please only type 'y' or 'n'")
+                break
+            else:
+                sys.exit()
+    except ValueError:
+        print("Please only type 'y' or 'n'")
+        time.sleep(3)
+        os.system('cls' if os.name == 'nt' else 'clear')
