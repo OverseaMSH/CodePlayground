3c71f4d420c81510b642f2b6f040189f66bd0117
Mohammad Sadegh Hemati <sadegh1384ordibehesht@gmail.com>
2024-06-12 00:04:39 +0330

    Almost solved the range error.

--- python/python-in-100-days-udemy/day12.py
+++ python/python-in-100-days-udemy/day12.py
@@ -1,5 +1,6 @@
 import time
 import random
+
 art = r"""
 000000    1111    222222    333333    44  44
 00  00      11         2        33    44  44
@@ -16,67 +17,58 @@ art = r"""
 print(art)
 print("Welcome to Sadegh python in 100 days day12 guess number game!\n")
 
+class RangeError(Exception):
+    pass
 
 def check_num(num, start, end, attempts):
-    while True and attempts > 0:
+    while attempts > 0:
         try:
             print(f"You have {attempts} remaining to guess the number")
             guess = int(input("Make a guess: "))
-            if guess:
-                if guess not in range(start, end):
-                    raise ("RangeError")
+            if guess < start or guess > end:
+                raise RangeError
+            else:
+                if num > guess:
+                    print("Too low!")
+                    attempts -= 1
+                elif num < guess:
+                    print("Too high!")
+                    attempts -= 1
                 else:
-                    if num > guess:
-                        print("Too low!")
-                        attempts -= 1
-                    elif num < guess:
-                        print("Too high!")
-                        attempts -= 1
-                    else:
-                        print(f"you got it! The answer was {guess}")
-                        break
-
+                    print(f"You got it! The answer was {guess}")
+                    break
         except ValueError:
-            print("Please enter an integer")
-        except ("RangeError"):
-            print(f"Please enter an integer in range of ({start},{end})")
-
+            print("Please enter an integer!")
+        except RangeError:
+            print(f"Please enter an integer in range of ({start},{end})!")
 
 while True:
     try:
-        numbersRanuge = input(
-            "Please enter a range (start,end) with a minimum difference of 15. For example (1,100): ")
-        start = int(numbersRanuge.strip("()").split(",")[0])
-        end = int(numbersRanuge.strip("()").split(",")[1])
-        if start and end and start < end:
-            if end - start >= 15:
-                while True:
-                    try:
-                        dif = input(
-                            "Choose a difficulty. Type 'easy' or 'hard': ").lower()
-                        if dif != "easy" and dif != "hard":
-                            raise ValueError
+        numbersRange = input("Please enter a range (start,end) with a minimum difference of 15. For example (1,100): ")
+        start = int(numbersRange.strip("()").split(",")[0])
+        end = int(numbersRange.strip("()").split(",")[1])
+        if start < end and end - start >= 15:
+            while True:
+                try:
+                    dif = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+                    if dif not in ["easy", "hard"]:
+                        raise ValueError
+                    else:
+                        num = random.randint(start, end)
+                        if dif == "easy":
+                            attempts = int((end - start) / 10)
                         else:
-                            if dif == "easy":
-                                attempts = int((end - start)/10)
-                                num = random.randint(start, end)
-                                check_num(num, start, end, attempts)
-                            else:
-                                attempts = int((end - start)/20)
-                                check_num(num, start, end, attempts)
-
-                    except ValueError:
-                        print("Please only type 'easy' or 'hard'")
-                        time.sleep(3)
-            else:
-                raise ("RangeError")
+                            attempts = int((end - start) / 20)
+                        check_num(num, start, end, attempts)
+                        break
+                except ValueError:
+                    print("Please only type 'easy' or 'hard'!")
+                    time.sleep(3)
         else:
-            raise ValueError
+            raise RangeError
     except ValueError:
-        print(
-            "Please enter a correct range (start,end) with integers. For example (1,100) ")
+        print("Please enter a correct range (start,end) with integers. For example (1,100)!")
         time.sleep(3)
-
-    except ("RangeError"):
-        print("Please enter a range with a minimum difference of 15")
+    except RangeError:
+        print(".Please enter a range with a minimum difference of 15!")
         time.sleep(3)
