7c9b7c0940e31d02581cbf284f118ff8773cf1b2
Mohammad Sadegh Hemati <sadegh1384ordibehesht@gmail.com>
2024-06-16 21:13:56 +0330

    Day14 is almost done but ut has a little problem about changes.

--- python/python-in-100-days-udemy/day14/day14.py
+++ python/python-in-100-days-udemy/day14/day14.py
@@ -1,85 +1,49 @@
 from db import data
-import random
-import time
-print(r"""
-  ___ ___  .__         .__                    
- /   |   \ |__|  ____  |  |__    ____ _______ 
-/    ~    \|  | / ___\ |  |  \ _/ __ \\_  __ \
-\    Y    /|  |/ /_/  >|   Y  \\  ___/ |  | \/
- \___|_  / |__|\___  / |___|  / \___  >|__|   
-    .__\/     /_____/       \/      \/        
-    |  |    ____ __  _  __  ____ _______      
-    |  |   /  _ \\ \/ \/ /_/ __ \\_  __ \     
-    |  |__(  <_> )\     / \  ___/ |  | \/     
-    |____/ \____/  \/\_/   \___  >|__|        
-                               \/      
-      """)
-print("Welcome to Sadegh python in 100 days day 14 higher lower game!")
+from art import logo, vs
+import random,os,sys
 
+print(logo)
+score = 0
+shouldContinue = True
+b = random.choice(data)
+data.remove(b)
 
-def compare_show(a, b, score):
+def format_account(a):
+    return f"{a['name']}, {a['description']}, from {a['country']}"
 
-    if score != 0:
-        a = b
-        b = random.choice(data)
-        data.remove(b)
-        print(f"Compare A: {a['name']}, {
-            a['description']} from {a['country']}.")
-        print(r"""
-        ___  ________    
-        \  \/ /  ___/    
-         \   /\___ \     
-          \_//____  > /\ 
-                  \/  \/ 
-          """)
-        print(f"Against B: {b['name']}, {
-              b['description']} from {b['country']}.")
+def check_answer(guess, a, b):
+    if a['follower_count'] > b['follower_count']:
+        return guess == "a"
     else:
-        a = random.choice(data)
-        data.remove(a)
-        print(f"Compare A: {a['name']}, {
-              a['description']} from {a['country']}.")
-        print(r"""
-        ___  ________    
-        \  \/ /  ___/    
-         \   /\___ \     
-          \_//____  > /\ 
-                  \/  \/ 
-          """)
-        b = random.choice(data)
-        data.remove(b)
-        print(f"Against B: {b['name']}, {
-              b['description']} from {b['country']}.")
-    return a, b
+        return guess == "b"
 
+while shouldContinue:
+    if len(data)==0:
+        print(f"Congratulation, All your guesses were Correct! You won with maximum score of {score}")
+        sys.exit()
+    print("Welcome to Sadegh python in 100 days day 14 higher lower game!")
+    a = b
+    b = random.choice(data)
+    data.remove(b)
 
-while len(data) > 0:
-    score = 0
-    while True:
-        try:
-            a, b = compare_show(a=False, b=False, score=0)
-            guess = input("Who has more followers? Type 'A' or 'B': ").lower()
-            if guess not in ['a', 'b']:
-                raise ValueError
-            else:
-                if guess == "a":
-                    if a['follower_count'] > b['follower_count']:
-                        score += 1
-                        print(f"You're right! Current score: {score}")
-                        time.sleep(3)
-                    else:
-                        print(
-                            f"Sorry, that's wrong. Game over! Final score: {score}")
-                        break
-                if guess == "b":
-                    if b['follower_count'] > a['follower_count']:
-                        score += 1
-                        print(f"You're right! Current score: {score}")
-                        time.sleep(3)
-                    else:
-                        print(
-                            f"Sorry, that's wrong. Game over! Final score: {score}")
-                        break
+    print(f"Compare A: {format_account(a)}")
+    print(vs)
+    print(f"Against B: {format_account(b)}")
 
-        except ValueError:
-            print("Type 'A' or 'B'! Try again!")
+    guess = ""
+    while guess not in ['a', 'b']:
+        guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+        if guess not in ['a', 'b']:
+            print("Invalid input. Please type 'A' or 'B' only!")
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    print(logo)
+
+    isCorrect = check_answer(guess, a, b)
+    if isCorrect:
+        score += 1
+        print(f"You're right! Current score: {score}")
+    else:
+        shouldContinue = False
+        print(f"Sorry, that's wrong. Final score: {score}")
