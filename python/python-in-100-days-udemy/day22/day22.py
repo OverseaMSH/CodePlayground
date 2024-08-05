0285da5978fc48dac8e384ee8ee163d9f37aa76f
Mohammad Sadegh Hemati <sadegh1384ordibehesht@gmail.com>
2024-08-05 14:58:49 +0330

    create paddles

--- python/python-in-100-days-udemy/day22/day22.py
+++ python/python-in-100-days-udemy/day22/day22.py
@@ -1,6 +1,29 @@
-from turtle import Screen
+from turtle import Screen, Turtle
+from paddle import Paddle
+# functions
+
+
+
+# functions
+# setup
 screen = Screen()
 screen.bgcolor("black")
-screen.setup(width=800,height=600)
+screen.setup(width=800, height=600)
 screen.title("Sadegh Udemy Python in 100 days Pong game")
+screen.tracer(0)
+# setup
+rPaddle = Paddle((350,0))
+lPaddle = Paddle((-350,0))
+
+
+screen.listen()
+screen.onkey(rPaddle.go_up, "Up")
+screen.onkey(rPaddle.go_down, "Down")
+screen.onkey(lPaddle.go_up, "w")
+screen.onkey(lPaddle.go_down, "s")
+game=True
+# setup
+while game:
+    screen.update()
 screen.exitonclick()
+# setup
