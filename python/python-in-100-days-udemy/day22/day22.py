6e47f91421ad802f8a6c2b51ddefa3760c6872ce
Mohammad Sadegh Hemati <sadegh1384ordibehesht@gmail.com>
2024-08-05 14:32:20 +0330

    base of day22 coded.

---
+++ python/python-in-100-days-udemy/day22/day22.py
@@ -0,0 +1,6 @@
+from turtle import Screen
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800,height=600)
+screen.title("Sadegh Udemy Python in 100 days Pong game")
+screen.exitonclick()
