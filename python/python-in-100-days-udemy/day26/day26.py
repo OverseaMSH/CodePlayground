0755a4bf059e7c3715b4062ca20e82fb4a8a387c
Mohammad Sadegh Hemati <sadegh1384ordibehesht@gmail.com>
2024-09-15 11:11:17 +0330

    csv to dict in day26

---
+++ python/python-in-100-days-udemy/day26/day26.py
@@ -0,0 +1,3 @@
+import pandas
+data=pandas.read_csv("python/python-in-100-days-udemy/day26/nato_phonetic_alphabet.csv").to_dict()
+
--- python/python-sabzlearn/passwordGenerator.py
+++ python/python-sabzlearn/passwordGenerator.py
@@ -4,13 +4,13 @@ lower = string.ascii_lowercase
 upper = string.ascii_uppercase
 digits = string.digits
 symbols = string.punctuation
-all=lower+upper+digits+symbols
+all = lower+upper+digits+symbols
 while True:
     print("Select one of the following options:\n\t1) Generate a password\n\t2) Exit the program")
     selection = input("Enter your selection: ")
     if selection == "1":
         length = int(input("Enter the length of the password: "))
-        password = "".join(random.sample(all,length))
+        password = "".join(random.sample(all, length))
         print(password)
     elif selection == "2":
         break
