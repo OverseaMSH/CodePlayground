294f9573b194a6f9d7cd40cdceaaea62dce0e9d3
Mohammad Sadegh Hemati <sadegh1384ordibehesht@gmail.com>
2024-09-15 11:33:06 +0330

    add error handling to day26 and finish it.

--- python/python-in-100-days-udemy/day26/day26.py
+++ python/python-in-100-days-udemy/day26/day26.py
@@ -1,10 +1,25 @@
-import pandas
-data = pandas.read_csv("python/python-in-100-days-udemy/day26/nato_phonetic_alphabet.csv")
+import pandas as pd
+import time
+data = pd.read_csv(
+    "python/python-in-100-days-udemy/day26/nato_phonetic_alphabet.csv")
+phoneticDict = {row.letter: row.code for (index, row) in data.iterrows()}
 while True:
     print("Welcome to Sadegh day26 udemy python in 100 days Nato Alphabet Project")
     name = input("What's your name?\n").upper()
-    phoneticDict = {row.letter: row.code for (index, row) in data.iterrows()}
-    # for k in list(name):
-    #     print(phoneticDict[k])
-    result=[phoneticDict[k] for k in name]
-    print(result)
+    if not name:
+        print("Empty input detected. Exiting program.")
+        time.sleep(3)
+        break
+    try:
+        if not name.isalpha():
+            raise ValueError("Name should contain only alphabetic characters.")
+
+        result = [phoneticDict[k] for k in name]
+        print(result)
+
+    except KeyError:
+        print("An error occurred: One or more letters are missing from the phonetic dictionary.")
+
+    except ValueError as ve:
+        print(f"Invalid input: {ve}. Exiting program.")
+        break
