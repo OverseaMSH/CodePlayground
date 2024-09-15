0b072d80d8b4d55ce1c87342e1652099d7957158
Mohammad Sadegh Hemati <sadegh1384ordibehesht@gmail.com>
2024-09-15 11:29:19 +0330

    Finished day26 with list and dict comprehension

--- python/python-in-100-days-udemy/day26/day26.py
+++ python/python-in-100-days-udemy/day26/day26.py
@@ -1,3 +1,10 @@
 import pandas
-data=pandas.read_csv("python/python-in-100-days-udemy/day26/nato_phonetic_alphabet.csv").to_dict()
-
+data = pandas.read_csv("python/python-in-100-days-udemy/day26/nato_phonetic_alphabet.csv")
+while True:
+    print("Welcome to Sadegh day26 udemy python in 100 days Nato Alphabet Project")
+    name = input("What's your name?\n").upper()
+    phoneticDict = {row.letter: row.code for (index, row) in data.iterrows()}
+    # for k in list(name):
+    #     print(phoneticDict[k])
+    result=[phoneticDict[k] for k in name]
+    print(result)
