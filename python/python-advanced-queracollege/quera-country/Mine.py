4ed0917ae8a0307fb9033cfcc9de0a6119a24b98
OverseaMSH <sadegh1384ordibehesht@gmail.com>
2024-09-17 08:56:24 +0330

    completed quera country project

---
+++ python/python-advanced-queracollege/quera-country/Mine.py
@@ -0,0 +1,16 @@
+from work_place import WorkPlace, Consts
+
+
+class Mine(WorkPlace):
+    def __init__(self, name):
+        super().__init__(name)
+        self.expertise = "mine"
+
+    def calc_capacity(self):
+        self.capacity = int(self.level ** 2)
+        return self.capacity
+
+    def calc_costs(self):
+        self.costs = int(Consts.BASE_PLACE_COST +
+                         Consts.LEVEL_MUL * self.level)
+        return self.costs
