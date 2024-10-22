b9a9700888c87e99cf12c5da532eb21a8a832d08
OverseaMSH <m.sadegh.hemati.dev@gmail.com>
2024-10-22 21:16:25 +0330

    Solved the problem with long long and a better loop condition.

--- queraQuestions/college/dataStructures&Algorithms/largetSubspace1.cpp
+++ queraQuestions/college/dataStructures&Algorithms/largetSubspace1.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-    long long max= -99999999;
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
-    for (int i=0;i<n;i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
-    
-    for (int i=0;i<n;i++){
-        int sum=0;
-        for(int j=0;j<n-i;j++){
-            sum+=arr[i+j];
-            if(sum>max){
-                max=sum;
+
+    long long maxSum = arr[0]; 
+    for (int i = 0; i < n; i++) {
+        long long sum = 0;
+        for (int j = i; j < n; j++) {
+            sum += arr[j]; 
+            if (sum > maxSum) {
+                maxSum = sum;
             }
         }
     }
-    cout<<max;
+
+    cout << maxSum << endl;
     return 0;
 }
