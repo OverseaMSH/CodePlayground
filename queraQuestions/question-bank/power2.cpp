6b142a858846db5c3119aa379010e19cdd4c825f
Mohammad Sadegh Hemati <sadegh1384ordibehesht@gmail.com>
2024-11-23 20:35:35 +0330

    Changed data type of 'n' and 'base' from int to long long, and modified while loop condition from '<' to '<='.

--- queraQuestions/question-bank/power2.cpp
+++ queraQuestions/question-bank/power2.cpp
@@ -2,9 +2,9 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int n;cin>>n;
-    int base=1;
-    while(base<n){
+    long long n;cin>>n;
+    long long base=1;
+    while(base<=n){
         base*=2;
     }
     cout<<base;
