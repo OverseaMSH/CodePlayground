de3cdecc9b2d09c7ebe2e524cadb1c3b365a132e
Mohammad Sadegh Hemati <sadegh1384ordibehesht@gmail.com>
2024-12-06 14:38:32 +0330

    Refactored buyBook.cpp: removed unnecessary code, added count variable, and modified loop logic.

--- queraQuestions/college/dataStructures&Algorithms/greedy-algorithms/buyBook.cpp
+++ queraQuestions/college/dataStructures&Algorithms/greedy-algorithms/buyBook.cpp
@@ -1,30 +1,40 @@
 // https://quera.org/college/3016/chapter/8236/lesson/167525
 #include <iostream>
+#include <vector>
 #include <algorithm>
+
 using namespace std;
-int main()
-{
-    int n, r;
-    cin >> n >> r;
-    int arr[n];
-    for (int i; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-    sort(arr, arr + n);
-    int sum=0;
-    for(int i=0;i<n;i++){
-        
-        sum+=arr[i];
-        if(sum>=r){
-            cout<<i;
-            break;
-        }else{
-            if (i==n-1){
-                cout<<i+1;
-            }
+
+int maxBooks(int n, long long r, vector<int>& prices) {
+    sort(prices.begin(), prices.end());
+    
+    int count = 0;  
+    long long remainingMoney = r; 
+    
+    for (int price : prices) {
+        if (remainingMoney >= price) {
+            count++; 
+            remainingMoney -= price;  
+        } else {
+            break;  
         }
-        
     }
     
+    return count;
+}
+
+int main() {
+    int n;
+    long long r;
+    cin >> n >> r;
+    
+    vector<int> prices(n);
+    
+    for (int i = 0; i < n; ++i) {
+        cin >> prices[i];
+    }
+    
+    cout << maxBooks(n, r, prices) << endl;
+    
+    return 0;
 }
