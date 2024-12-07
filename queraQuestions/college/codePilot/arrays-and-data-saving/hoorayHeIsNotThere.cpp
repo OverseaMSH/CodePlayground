73c61afafe5244085612be058add732e03ef81c6
Mohammad Sadegh Hemati <sadegh1384ordibehesht@gmail.com>
2024-12-07 17:45:42 +0330

    Solved HoorayHeIsNotThere.

---
+++ queraQuestions/college/codePilot/arrays-and-data-saving/hoorayHeIsNotThere.cpp
@@ -0,0 +1,34 @@
+// https : // quera.org/college/4499/chapter/12639/lesson/42889/
+#include <iostream>
+#include <set>
+#include <string>
+        using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+    string aS[a];
+    set<string> days;
+    for (int i = 0; i < a; i++)
+    {
+        cin >> aS[i];
+        days.insert(aS[i]);
+    }
+    int b;
+    cin >> b;
+    string bS[b];
+    for (int i = 0; i < b; i++)
+    {
+        cin >> bS[i];
+        days.insert(bS[i]);
+    }
+    int c;
+    cin >> c;
+    string cS[c];
+    for (int i = 0; i < c; i++)
+    {
+        cin >> cS[i];
+        days.insert(cS[i]);
+    }
+    cout << 7 - days.size();
+}
