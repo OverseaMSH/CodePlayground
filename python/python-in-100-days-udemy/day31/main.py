c40934e4e8e033b63af89e21a1bed2c52bbc4597
Mohammad Sadegh Hemati <sadegh1384ordibehesht@gmail.com>
2024-12-15 09:13:53 +0330

    Add Flashcard functionality: Implement next_card function to display random French words from CSV

--- python/python-in-100-days-udemy/day31/main.py
+++ python/python-in-100-days-udemy/day31/main.py
@@ -1,4 +1,17 @@
 from tkinter import *
+import pandas
+import random
+data = pandas.read_csv(
+    "python/python-in-100-days-udemy/day31/data/french_words.csv")
+data = data.to_dict(orient="records")
+
+
+def next_card():
+    currentCard = random.choice(data)
+    canvas.itemconfig(cardTitle, text="French")
+    canvas.itemconfig(cardWord, text=currentCard["French"])
+
+
 BACKGROUND_COLOR = "#B1DDC6"
 window = Tk()
 window.title("Sadegh day31 Flashcard App")
@@ -6,17 +19,20 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526)
 cardFrontImg = PhotoImage(
     file="python/python-in-100-days-udemy/day31/images/card_front.png")
-canvas.create_image(400,263,image=cardFrontImg)
-canvas.create_text(400,150,text="Title",font=("Ariel",40,"italic"))
-canvas.create_text(400,263,text="word",font=("Ariel",60,"bold"))
-canvas.config(bg=BACKGROUND_COLOR,highlightthickness=0)
-canvas.grid(row=0,column=0,columnspan=2)
-crossImage=PhotoImage(
+canvas.create_image(400, 263, image=cardFrontImg)
+cardTitle = canvas.create_text(
+    400, 150, text="Title", font=("Ariel", 40, "italic"))
+cardWord = canvas.create_text(
+    400, 263, text="word", font=("Ariel", 60, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
+crossImage = PhotoImage(
     file="python/python-in-100-days-udemy/day31/images/wrong.png")
-dontKnowBtn=Button(image=crossImage)
-dontKnowBtn.grid(row=1,column=0)
-checkImage=PhotoImage(
+dontKnowBtn = Button(image=crossImage, highlightthickness=0,border=0, command=next_card)
+dontKnowBtn.grid(row=1, column=0)
+checkImage = PhotoImage(
     file="python/python-in-100-days-udemy/day31/images/right.png")
-knownBtn=Button(image=checkImage)
-knownBtn.grid(row=1,column=1)
+knownBtn = Button(image=checkImage, highlightthickness=0,border=0, command=next_card)
+knownBtn.grid(row=1, column=1)
+next_card()
 window.mainloop()
