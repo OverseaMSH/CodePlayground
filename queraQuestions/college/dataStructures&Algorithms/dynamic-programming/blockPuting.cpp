b48218af33e93fe9149288cd20b20ecd19b062fb
Mohammad Sadegh Hemati <sadegh1384ordibehesht@gmail.com>
2024-12-24 23:51:31 +0330

    Refactor blockPuting.cpp: optimize precomputation logic and improve readability by adjusting constants and handling edge cases. Solved.

--- queraQuestions/college/dataStructures&Algorithms/dynamic-programming/blockPuting.cpp
+++ queraQuestions/college/dataStructures&Algorithms/dynamic-programming/blockPuting.cpp
@@ -1,9 +1,9 @@
-
+https://quera.org/college/3016/chapter/8238/lesson/27723/
 #include <iostream>
 using namespace std;
 
-const int MOD = 1000000007;
-const int MAX_N = 100000;
+const int MOD = 1e9 + 7;
+const int MAX_N = 1e5;
 
 long long f[MAX_N + 1];
 
@@ -12,23 +12,22 @@ void precompute() {
     f[1] = 1;
     f[2] = 1;
     f[3] = 2;
-    
     for (int i = 4; i <= MAX_N; i++) {
-        f[i] = (f[i - 1] + f[i - 2] + f[i - 3] - f[i - 4]) % MOD;
+        f[i] = (f[i - 1] + f[i - 2] + f[i - 3]) % MOD;
+        if (i - 4 >= 0) {
+            f[i] = (f[i] - f[i - 4] + MOD) % MOD;
+        }
     }
 }
 
 int main() {
     int q;
     cin >> q;
-    
     precompute();
-    
     while (q--) {
         int n;
         cin >> n;
         cout << f[n] << endl;
     }
-    
     return 0;
 }
