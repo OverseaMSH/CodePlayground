0d2c0753bd2e95ee2ba0d0905f4377e9184f7a8d
Mohammad Sadegh Hemati <sadegh1384ordibehesht@gmail.com>
2024-12-24 20:05:50 +0330

    Add password.cpp for calculating the minimum number of rotations needed to match a password with given inputs.

---
+++ queraQuestions/college/codePilot/algorithms-introduction/password.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int k;
+    cin >> k;
+    string pass;
+    cin >> pass;
+    int sum = 0;
+    for (int i = 0; i < k; i++) {
+        string n;
+        cin >> n;
+        int j = 0;
+        while (n[j] != pass[i]) {
+            j++;
+        }
+        sum += min(j, 9 - j);
+    }
+    cout << sum << endl;
+    return 0;
+}
