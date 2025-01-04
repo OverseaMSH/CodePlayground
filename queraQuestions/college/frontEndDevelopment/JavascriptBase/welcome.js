4c45500ef23c049754aec708cfa6dc8d814914ff
Mohammad Sadegh Hemati <sadegh1384ordibehesht@gmail.com>
2025-01-04 19:26:05 +0330

    Add readline-sync dependency to start js base with welcome.js

---
+++ queraQuestions/college/frontEndDevelopment/JavascriptBase/welcome.js
@@ -0,0 +1,5 @@
+// https://quera.org/college/6092/chapter/20016/lesson/64888/?comments_page=1&comments_filter=ALL&submissions_page=1
+// const r = require('readline-sync');
+// function readline() {return r.question();}
+let name=readline();
+console.log("Hello, "+name+"!");
