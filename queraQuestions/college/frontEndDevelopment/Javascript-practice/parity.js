5b36fba7cbd2fde4099af8b362f68abfe6cab8d7
Mohammad Sadegh Hemati <sadegh1384ordibehesht@gmail.com>
2025-01-11 01:21:54 +0330

    Add implementations for backFromPark and parity JavaScript challenges. Solved.

---
+++ queraQuestions/college/frontEndDevelopment/Javascript-practice/parity.js
@@ -0,0 +1,9 @@
+// https://quera.org/college/6092/chapter/73476/lesson/138793/?comments_page=1&comments_filter=ALL&submissions_page=1
+let a= +readline()
+let b= +readline()
+let c= +readline()
+if (a%2===0 || b%2===0 && c%2===0){
+    console.log("good")
+}else{
+    console.log("bad")
+}
